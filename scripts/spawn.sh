768a8cd114bba9cc1b49badbf0c588abcb5ac214
David Martinez <contacto@davidmartinez.net>
2017-04-24 23:47:35 +0200

    Added 1.14 to supported versions

--- scripts/spawn.sh
+++ scripts/spawn.sh
@@ -2,7 +2,7 @@
 
 JARS=${APACHE_TIKA_JARS:-bin}
 
-declare -a SUPPORTED_VERSIONS=("1.13" "1.12" "1.11" "1.10" "1.9" "1.8" "1.7")
+declare -a SUPPORTED_VERSIONS=("1.14" "1.13" "1.12" "1.11" "1.10" "1.9" "1.8" "1.7")
 
 PORT=9998
 for VERSION in "${SUPPORTED_VERSIONS[@]}"
@@ -10,9 +10,9 @@ do
     RUNNING=`ps aux | grep -c tika-server-$VERSION`
 
     if [ $RUNNING -lt 2 ]; then
-        java -jar "$JARS/tika-server-$VERSION.jar" -p $PORT 2> /tmp/tika-server-$VERSION.log &
+        echo java -jar "$JARS/tika-server-$VERSION.jar" -p $PORT 2> /tmp/tika-server-$VERSION.log &
         ((PORT++))
-        sleep 2
+        #sleep 2
     else
         echo "Tika Server $VERSION already running"
     fi
