1cc520ddd7198da4f8a0ae8e4b2875128a17fed9
David Martínez <contacto@davidmartinez.net>
2017-05-31 01:01:04 +0200

    Download script creates target if not exists

--- scripts/download.sh
+++ scripts/download.sh
@@ -4,6 +4,8 @@ BINARIES=${APACHE_TIKA_BINARIES:-bin}
 
 declare -a SUPPORTED_VERSIONS=("1.7" "1.8" "1.9" "1.10" "1.11" "1.12" "1.13" "1.14" "1.15")
 
+mkdir --parents $BINARIES
+
 for VERSION in "${SUPPORTED_VERSIONS[@]}"
 do
    if [ ! -f "$BINARIES/tika-app-$VERSION.jar" ]; then
