48dc4c4d753e1c3b821df31e5eefd81bdd99238a
David Martínez <vaites@users.noreply.github.com>
2017-05-31 01:27:11 +0200

    Latest version cannot be ownloaded from archive

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+LATEST="1.15"
 BINARIES=${APACHE_TIKA_BINARIES:-bin}
 
 declare -a SUPPORTED_VERSIONS=("1.7" "1.8" "1.9" "1.10" "1.11" "1.12" "1.13" "1.14" "1.15")
@@ -8,11 +9,17 @@ mkdir --parents $BINARIES
 
 for VERSION in "${SUPPORTED_VERSIONS[@]}"
 do
+   if [ $VERSION == $LATEST ]; then
+       MIRROR="http://www-us.apache.org" 
+   else
+       MIRROR="https://archive.apache.org"
+   fi
+
    if [ ! -f "$BINARIES/tika-app-$VERSION.jar" ]; then
-        wget "https://archive.apache.org/dist/tika/tika-app-$VERSION.jar" -O "$BINARIES/tika-app-$VERSION.jar"
+        wget "$MIRROR/dist/tika/tika-app-$VERSION.jar" -O "$BINARIES/tika-app-$VERSION.jar"
    fi
 
    if [ ! -f "$BINARIES/tika-server-$VERSION.jar" ]; then
-        wget "https://archive.apache.org/dist/tika/tika-server-$VERSION.jar" -O "$BINARIES/tika-server-$VERSION.jar"
+        wget "$MIRROR/dist/tika/tika-server-$VERSION.jar" -O "$BINARIES/tika-server-$VERSION.jar"
    fi
 done
