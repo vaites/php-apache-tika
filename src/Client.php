6bd15a1308a5c23a62d44fc28267a74b25d5b186
David Martinez <contacto@davidmartinez.net>
2018-03-07 02:26:37 +0100

    Tested up to version 1.17

--- src/Client.php
+++ src/Client.php
@@ -22,7 +22,10 @@ abstract class Client
      *
      * @var array
      */
-    protected static $supportedVersions = ['1.7', '1.8', '1.9', '1.10', '1.11', '1.12', '1.13', '1.14', '1.15', '1.16'];
+    protected static $supportedVersions =
+    [
+        '1.7', '1.8', '1.9', '1.10', '1.11', '1.12', '1.13', '1.14', '1.15', '1.16', '1.17'
+    ];
 
     /**
      * Response using callbacks
