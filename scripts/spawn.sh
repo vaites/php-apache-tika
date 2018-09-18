40ef3758bd81d40380ea3b740b215407f979ec57
David Martinez <contacto@davidmartinez.net>
2018-09-19 01:41:52 +0200

    Added '-enableUnsecureFeatures -enableFileUrl' to spawn.sh script

--- scripts/spawn.sh
+++ scripts/spawn.sh
@@ -9,7 +9,7 @@ RUNNING=`ps aux | grep -c tika-server-$VERSION`
 if [ $RUNNING -lt 2 ]; then
     java -version
     echo "Starting Tika Server $VERSION"
-    java -jar "$BINARIES/tika-server-$VERSION.jar" -p $PORT 2> /tmp/tika-server-$VERSION.log &
+    java -jar "$BINARIES/tika-server-$VERSION.jar" -p $PORT -enableUnsecureFeatures -enableFileUrl 2> /tmp/tika-server-$VERSION.log &
     ((PORT++))
     sleep 5
 else
