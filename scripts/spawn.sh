9061985a2d2c7db0c8dcee5706e52b2d5d8ec1bb
David Martinez <contacto@davidmartinez.net>
2018-09-21 00:16:40 +0200

    The 'enableUnsecureFeatures' is not available for Tika before 1.14 version

--- scripts/spawn.sh
+++ scripts/spawn.sh
@@ -4,14 +4,31 @@ PORT=9998
 BINARIES=${APACHE_TIKA_BINARIES:-bin}
 VERSION=${APACHE_TIKA_VERSION:-"1.19"}
 
-RUNNING=`ps aux | grep -c tika-server-$VERSION`
+RUNNING=$(ps aux | grep -c tika-server-$VERSION)
 
 if [ $RUNNING -lt 2 ]; then
     java -version
     echo "Starting Tika Server $VERSION"
-    java -jar "$BINARIES/tika-server-$VERSION.jar" -p $PORT -enableUnsecureFeatures -enableFileUrl 2> /tmp/tika-server-$VERSION.log &
-    ((PORT++))
-    sleep 5
+
+    if [ "$1" == "--foreground" ]; then
+        MODE="foreground"
+    else
+        MODE="background"
+    fi
+
+    if [ $(echo "$VERSION > 1.14" | bc) -gt 0 ]; then
+        COMMAND="java -jar $BINARIES/tika-server-$VERSION.jar -p $PORT -enableUnsecureFeatures -enableFileUrl"
+    else
+        COMMAND="java -jar $BINARIES/tika-server-$VERSION.jar -p $PORT"
+    fi
+
+    if [ $MODE == "background" ]; then
+        $COMMAND  2> /tmp/tika-server-$VERSION.log &
+        sleep 5
+    else
+        $COMMAND
+    fi
+
 else
     echo "Tika Server $VERSION already running"
 fi
